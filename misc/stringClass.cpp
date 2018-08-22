694e6b83cac6fe274621f9a9e942fb3f8a8a8536
ysripath <yuvarajsripathi91@gmail.com>
2018-08-22 21:36:24 +0000

    First commit for custom string implementation
    Signed-off-by: ysripath <yuvarajsripathi91@gmail.com>

---
+++ misc/stringClass.cpp
@@ -0,0 +1,5 @@
+#include "stringClass.h"
+
+class stringC {
+
+};
---
+++ misc/stringClass.h
@@ -0,0 +1,9 @@
+class stringC
+{
+	private:
+		// Character array for storung the data
+	public:
+		int length; // Length o fthe string
+		bool isEmpty(); // Check if string is empty or not
+		
+};
